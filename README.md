1111cfee24ddd384abfea637af38ef709a2573f2
Alan Claughan <git@claughan.com>
2021-04-12 06:51:31 +0200

    now keeps track of your progress

---
+++ README.md
@@ -0,0 +1,13 @@
+## flash cards
+
+### description:
+
+flash_cards is designed to help you learn something. In this case its French. The words to learn are kept in '
+data/french_words'.<p> The French word is displayed first and then after three seconds the English word is displayed.<p>
+If you got the answer correct, click the green button, else click the red button.<p>The known words are removed from the
+list of words and the remaining words are saved to 'data/words_to_learn.csv'
+<br>
+
+### screenshot
+
+![screenshot](screenshot.png)
--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-from tkinter import *
-import pandas
 from random import choice
+from tkinter import *
+
+import pandas
 
 BACKGROUND_COLOUR = "#424242"
 
-data = pandas.read_csv("data/french_words.csv")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+
 to_learn = data.to_dict(orient="records")
-
-
-
+current_word = {}
+flip_time = 3000
 
 
 def next_card():
-    word = choice(to_learn)['French']
-    canvas.itemconfig(card_word, text=word)
+    global current_word, flip_time
+    win.after_cancel(flip_time)
+    current_word = choice(to_learn)
+    canvas.itemconfig(card_bg, image=flash_card_f)
+    canvas.itemconfig(card_word, text=current_word['French'])
+    flip_time = win.after(3000, func=change_card)
+
+
+def correct():
+    to_learn.remove(current_word)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+def change_card():
+    canvas.itemconfig(card_bg, image=flash_card_e)
+    canvas.itemconfig(card_word, text=current_word['English'])
+
 
 # build GUI
-
 win = Tk()
 win.title("Flashy")
 win.config(
@@ -25,12 +45,13 @@ win.config(
     bg=BACKGROUND_COLOUR
 )
 
+flip_time = win.after(3000, func=change_card)
+
 # image
 flash_card_f = PhotoImage(file="images/french.png")
 flash_card_e = PhotoImage(file="images/english.png")
 key_tick = PhotoImage(file="images/key_1.png")
 key_cross = PhotoImage(file="images/key_2.png")
-
 
 canvas = Canvas(
     width=800,
@@ -39,7 +60,7 @@ canvas = Canvas(
     highlightthickness=0
 )
 
-canvas.create_image(
+card_bg = canvas.create_image(
     400, 268,
     image=flash_card_f,
 )
@@ -48,30 +69,29 @@ canvas.grid(column=1, row=0, columnspan=2)
 
 # text
 card_word = canvas.create_text(
-    400,263,
-    text="word",
+    400, 263,
+    text='',
     font=("Arial", 80, "bold")
 )
 
-
 # buttons
 button_tick = Button(
-    image = key_tick,
+    image=key_tick,
     highlightthickness=0,
     bg=BACKGROUND_COLOUR,
-    command=next_card
+    command=correct
 )
 
-button_tick.grid(column=1,row=1)
+button_tick.grid(column=1, row=1)
 
 button_cross = Button(
-    image = key_cross,
+    image=key_cross,
     highlightthickness=0,
     bg=BACKGROUND_COLOUR,
     command=next_card
 )
 
-button_cross.grid(column=2,row=1)
+button_cross.grid(column=2, row=1)
 
 next_card()
 
